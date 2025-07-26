65836bf0e2224eb86dd43174cb9f626bdfa7f690
Bryan Khor <bryankhor07@gmail.com>
2025-07-26 13:38:19 -0700

    Updating my store with the useDispatch hook.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
-import productList from '../data/productList.json'
-import '../styles/home.scss'
+import { useDispatch } from "react-redux";
+import productList from "../data/productList.json";
+import cartSlice from "../data/cartSlice";
+import "../styles/home.scss";
 
 const Home = () => {
+  const { addToCart, removeFromCart } = cartSlice.actions;
+  const dispatch = useDispatch();
+
   return (
     <div className="container product-catalogue">
       <div className="row">
@@ -9,21 +14,36 @@ const Home = () => {
           return (
             <div className="wrapper col-md-4" key={product.id}>
               <div className="card">
-                <img className="card-img-top center-block" src={product.imageUrl} alt="Card cap" />
+                <img
+                  className="card-img-top center-block"
+                  src={product.imageUrl}
+                  alt="Card cap"
+                />
 
                 <div className="card-body text-center">
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  <button className="btn btn-primary">Add to cart</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => dispatch(addToCart(product.id))}
+                  >
+                    Add to cart
+                  </button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => dispatch(removeFromCart(product.id))}
+                  >
+                    Remove from cart
+                  </button>
                 </div>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
